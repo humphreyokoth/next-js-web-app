d5664869c0df0bd5f803075f6f818740315b7847
humphreyokoth <humpasp@gmail.com>
2022-06-08 15:56:06 +0300

    Navbar

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h3>About</h3>
+    </div>
+  );
+};
+
+export default about;
