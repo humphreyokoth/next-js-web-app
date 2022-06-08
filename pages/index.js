77bd240728a74f87512d638c575214aafb1dcdf8
humphreyokoth <humpasp@gmail.com>
2022-06-08 21:57:43 +0300

    articlelist

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,35 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import styles from '../styles/Layout.module.css'
+import styles from "../styles/Layout.module.css";
 
-export default function Home({articles}) {
- console.log(articles);
+export default function Home({ articles }) {
+  //  console.log(articles);
   return (
     <div>
-    <Head>
-      <title>
-        EduNext JS
-        <meta name='keywords' content='edu outbox ,web development, programming'/>
-      </title>
-    </Head>
+      <Head>
+        <title>EduNext JS</title>
+        <meta
+          name="keywords"
+          content="edu outbox ,web development, programming"
+        />
+      </Head>
       <h1>EDU NEXT JS CRUSH COURSE</h1>
-   {/* {articles.map((article)=> (<h3>{article.title}</h3>))} */}
+      {articles.map((article) => (
+        <h3>{article.title}</h3>
+      ))}
     </div>
-  )
+  );
 }
 
+export const getStatciProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
 
-export const getStatciProps = async () =>{
-  const res = await fetch (`https://jsonplaceholder.typicode.com/posts?_limit=6`)
-
-  const articles = await res.json()
+  const articles = await res.json();
   return {
-    props:{
-      articles
-    }
-  }
-}
+    props: {
+      articles,
+    },
+  };
+};
